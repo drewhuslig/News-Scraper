2c05109a1e879734c24d53b3f2ec039f714d9ef8
Drew Huslig <drewhuslig@gmail.com>
2017-12-13 15:26:24 -0600

    remove test/test.js

--- test/test.js
+++
@@ -1,8 +0,0 @@
-require("../config/connection");
-const request = require("request");
-const cheerio = require("cheerio");
-const Article = require("../models/article");
-const Comment = require("../models/comment");
-
-console.log("here");
-console.log(Article.find({ title: "How to Cook Turkey" }));
